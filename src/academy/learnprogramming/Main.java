f5e75d69fa85062b97e5120f3a12fa9f4db2cd57
ryoray <praretno.wibowo@gmail.com>
2020-05-21 14:52:27 +0700

    Adding Tim's solution

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -3,9 +3,33 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-        Printer printer = new Printer(40,true);
+        Printer printer = new Printer(50,false);
+        System.out.println("Initial page count= " + printer.getPagePrinted());
 
-        printer.printPage(79,300,true);
-        printer.fillToner(-20);
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted +
+                " new total of page count = " + printer.getPagePrinted() );
+
+        pagesPrinted = printer.printPages(10);
+        System.out.println("Pages printed was " + pagesPrinted +
+                " new total of page count = " + printer.getPagePrinted() );
+
+        printer.setPagePrinted(0);
+
+        Printer printer1 = new Printer(50,true);
+        System.out.println("Initial page count= " + printer.getPagePrinted());
+
+        pagesPrinted = printer1.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted +
+                " new total of page count = " + printer1.getPagePrinted() );
+
+        pagesPrinted = printer1.printPages(10);
+        System.out.println("Pages printed was " + pagesPrinted +
+                " new total of page count = " + printer1.getPagePrinted() );
+
+//        Printer printer = new Printer(40,true);
+//
+//        printer.printPage(79,600,false);
+//        printer.fillToner(20);
     }
 }
--- src/academy/learnprogramming/Printer.java
+++ src/academy/learnprogramming/Printer.java
@@ -3,52 +3,97 @@ package academy.learnprogramming;
 public class Printer {
 
     private int tonerLevel;
-    private int tonerUsage;
-    private boolean isDuplex;
+    private int pagePrinted;
+    private boolean duplex;
 
-
-    public Printer(int tonerLevel, boolean isDuplex) {
-        this.tonerLevel = tonerLevel;
-        this.isDuplex = true;
+    public Printer(int tonerLevel, boolean duplex) {
+        if (tonerLevel > -1 && tonerLevel <= 100) {
+            this.tonerLevel = tonerLevel;
+        }else {
+            this.tonerLevel = -1;
+        }
+        this.duplex = duplex;
+        this.pagePrinted = 0;
     }
 
-    public void fillToner(int addToner) {
-        if (addToner < 0) {
-            System.out.println("You can't input negative amount");
-        } else {
-            if (this.getTonerLevel() < 50) {
-                if (this.getTonerLevel() + addToner > 100) {
-                    System.out.println("You add too much toner. You just need to add " +
-                            (100 - getTonerLevel()) + "% toner");
-                } else {
-                    this.tonerLevel += addToner;
-                    System.out.println("You add " + addToner + "% toner. Current toner level is now " + this.getTonerLevel() + " %");
-                }
-            } else {
-                System.out.println("You still have enough toner.");
+    public int addToner(int tonerAmount) {
+        if (tonerAmount > 0 && tonerAmount <= 100) {
+            if (this.tonerLevel + tonerAmount > 100) {
+                return -1;
             }
-        }
-    }
-
-    public int printPage(int tonerLevel, int pages, boolean isDuplex) {
-        if (isDuplex) {
-            tonerUsage = pages / 10;
+            this.tonerLevel += tonerAmount;
+            return this.tonerLevel;
         } else {
-            tonerUsage = pages / 20;
+            return -1;
         }
-        this.tonerLevel = tonerLevel - tonerUsage;
-        System.out.println("You have printed " + pages + " pages. You use " + getTonerUsage() +
-                "% of toner level");
-        return this.tonerLevel;
     }
 
-
-    public int getTonerLevel() {
-        return tonerLevel;
+    public int printPages(int pages) {
+        int pagesToPrint = pages;
+        if (this.duplex) {
+            pagesToPrint = (pages / 2) + (pages % 2);
+            System.out.println("Printing in duplex mode");
+        }
+        this.pagePrinted += pagesToPrint;
+        return pagesToPrint;
     }
 
-    public int getTonerUsage() {
-        return tonerUsage;
+    public int getPagePrinted() {
+        return pagePrinted;
     }
+
+    public void setPagePrinted(int pagePrinted) {
+        this.pagePrinted = pagePrinted;
+    }
+
+    //    private int tonerLevel;
+//    private int tonerUsage;
+//    private boolean isDuplex;
+//
+//    public Printer(int tonerLevel, boolean isDuplex) {
+//        this.tonerLevel = tonerLevel;
+//        this.isDuplex = true;
+//    }
+//
+//    public void fillToner(int addToner) {
+//        if (addToner < 0) {
+//            System.out.println("You can't input negative amount");
+//        } else {
+//            if (this.getTonerLevel() < 50) {
+//                if (this.getTonerLevel() + addToner > 100) {
+//                    System.out.println("You add too much toner. You just need to add " +
+//                            (100 - getTonerLevel()) + "% toner");
+//                } else {
+//                    this.tonerLevel += addToner;
+//                    System.out.println("You add " + addToner + "% toner. Current toner " +
+//                            "level is now " + this.getTonerLevel() + " %");
+//                }
+//            } else {
+//                System.out.println("You still have enough toner.");
+//            }
+//        }
+//    }
+//
+//    public int printPage(int tonerLevel, int pages, boolean isDuplex) {
+//        this.isDuplex = isDuplex;
+//        if (this.isDuplex) {
+//            tonerUsage = pages / 10;
+//        } else {
+//            tonerUsage = pages / 20;
+//        }
+//        this.tonerLevel = tonerLevel - tonerUsage;
+//        System.out.println("You have printed " + pages + " pages. You use " + getTonerUsage() +
+//                "% of toner level");
+//        return this.tonerLevel;
+//    }
+//
+//
+//    public int getTonerLevel() {
+//        return tonerLevel;
+//    }
+//
+//    public int getTonerUsage() {
+//        return tonerUsage;
+//    }
 
 }
